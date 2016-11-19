bc3434fb1b2167c1ee0e41172d56d0f2f207475d
Anton Mamkin <mail@tbrd.ru>
2016-11-19 03:55:59 +0300

    3a

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const saveData = require("./saveData");
-const Pet = require("./models/Pet");
-const User = require("./models/User");
-const isAdmin = require("./middlewares/isAdmin");
-mongoose.connect('mongodb://localhost/test');
-mongoose.Promise = Promise;
+const request = require("request-promise");
 const allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://account.skill-branch.ru");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -24,38 +17,41 @@ const allowCrossDomain = function (req, res, next) {
 };
 let app = express();
 app.use(allowCrossDomain);
-app.use(bodyParser.json());
-app.get("/clear", isAdmin, (req, res) => __awaiter(this, void 0, void 0, function* () {
-    yield User.remove({});
-    yield Pet.remove({});
-    return res.send("OK");
+let pc = {};
+const pcUrl = "https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json";
+request(pcUrl)
+    .then((res) => __awaiter(this, void 0, void 0, function* () {
+    pc = yield JSON.parse(res);
+}))
+    .catch(err => {
+    console.log("Что-то пошло не так", err);
+});
+app.get("/volumes", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    let disk = {};
+    let hdd = pc["hdd"];
+    for (let key of hdd) {
+        if (!(key['volume'] in disk))
+            disk[key['volume']] = '0B';
+        disk[key['volume']] = disk[key['volume']].replace(/^\d*/, +disk[key['volume']].slice(0, -1) + key['size']);
+    }
+    res.send(disk);
 }));
-app.post("/data", (req, res) => __awaiter(this, void 0, void 0, function* () {
-    const data = req.body;
-    if (!data.user)
-        return res.status(400).send("user required");
-    if (!data.pets)
-        data.pets = [];
-    const user = yield User.findOne({
-        name: data.user.name
+app.get("/*", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    let url = req.url;
+    url = url.replace(/^\/|\/$/g, '');
+    let path = url.split('/');
+    if (url == '')
+        path = [];
+    let answer = pc;
+    path.forEach((key, index) => {
+        if ((key == 'length' && index == 0) || (key != 'length' && key in answer)) {
+            answer = answer[key];
+        }
+        else {
+            res.status(404).send("Not Found");
+        }
     });
-    if (user)
-        return res.status(400).send("user.name is exists");
-    try {
-        const result = yield saveData(data);
-        return res.json(result);
-    }
-    catch (err) {
-        return res.status(500).json(err);
-    }
-}));
-app.get("/users", (req, res) => __awaiter(this, void 0, void 0, function* () {
-    const users = yield User.find();
-    res.json(users);
-}));
-app.get("/pets", (req, res) => __awaiter(this, void 0, void 0, function* () {
-    const pets = yield Pet.find().populate("owner");
-    res.json(pets);
+    res.json(answer);
 }));
 app.listen(3000, function () {
     console.log("Example app listening on port 3000!");
--- index.ts
+++ index.ts
@@ -1,20 +1,10 @@
 import express = require("express");
-import mongoose = require("mongoose");
-import bodyParser = require("body-parser");
-// import * as prm from 'bluebird';
-import saveData = require("./saveData");
-import Pet = require("./models/Pet");
-import User = require("./models/User");
-
-import isAdmin = require("./middlewares/isAdmin");
-
+import request = require("request-promise");
+import xregexp = require("xregexp");
 
 import Request = express.Request;
 import Response = express.Response;
 import NextFunction = express.NextFunction;
-
-mongoose.connect('mongodb://localhost/test');
-mongoose.Promise = Promise;
 
 // CORS middleware
 const allowCrossDomain = function(req: Request, res: Response, next: NextFunction) {
@@ -27,43 +17,44 @@ const allowCrossDomain = function(req: Request, res: Response, next: NextFunctio
 
 let app = express();
 app.use(allowCrossDomain);
-app.use(bodyParser.json());
 
-app.get("/clear", isAdmin, async (req: Request, res: Response) => {
-  await User.remove({});
-  await Pet.remove({});
-  return res.send("OK");
-});
+let pc = {};
+const pcUrl = "https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json";
+request(pcUrl)
+  .then(async (res: any) => {
+    pc = await JSON.parse(res);
+  })
+  .catch(err => {
+    console.log("Что-то пошло не так", err);
+  })
 
-app.post("/data", async (req: Request, res: Response) => {
-  const data = req.body;
-  if(!data.user) return res.status(400).send("user required");
-  if(!data.pets) data.pets = [];
-
-  const user = await User.findOne({
-    name: data.user.name
-  });
-
-  if (user) return res.status(400).send("user.name is exists");
-
-  try {
-    const result = await saveData(data);
-    return res.json(result);
-  } catch (err) {
-    return res.status(500).json(err);
+app.get("/volumes", async (req: Request, res: Response) => {
+  let disk = {};
+  let hdd = pc["hdd"];
+  for (let key of hdd) {
+    if (!(key['volume'] in disk)) disk[key['volume']] = '0B';
+    disk[key['volume']] = disk[key['volume']].replace(/^\d*/, +disk[key['volume']].slice(0, -1) + key['size']);
   }
+  res.send(disk);
 });
 
+app.get("/*", async (req: Request, res: Response) => {
+  let url = req.url;
+  url = url.replace(/^\/|\/$/g, '');
+  let path = url.split('/');
+  if (url == '') path = [];
 
-app.get("/users", async (req: Request, res: Response) => {
-  const users = await User.find();
-  res.json(users);
+  let answer = pc;
+  path.forEach((key, index) => {
+    if ((key == 'length' && index == 0) || (key != 'length' && key in answer)) {
+      answer = answer[key];
+    } else {
+      res.status(404).send("Not Found");
+    }
+  });
+  res.json(answer);
 });
 
-app.get("/pets", async (req: Request, res: Response) => {
-  const pets = await Pet.find().populate("owner");
-  res.json(pets);
-});
 
 app.listen(3000, function () {
     // tslint:disable-next-line
